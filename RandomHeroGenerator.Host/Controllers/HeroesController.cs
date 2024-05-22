3977e0cad440d65f771dbaa8411351c3d43e7c91
jazal ahamed <jazalahamed19@gmail.com>
2024-05-23 00:19:48 +0530

    After refactroing the code it works fine

--- RandomHeroGenerator.Host/Controllers/HeroesController.cs
+++ RandomHeroGenerator.Host/Controllers/HeroesController.cs
@@ -112,55 +112,7 @@ namespace RandomHeroGenerator.Host.Controllers
 
         private static void SimulateAttack(Hero attacker, Hero defender)
         {
-            bool? attackSuccess;
-
             HeroHelper.GetTheWinner(attacker, defender);
-
-            //switch (attacker.Type)
-            //{
-            //    case "Archer":
-            //        attackSuccess = defender.Type switch
-            //        {
-            //            "Horseman" => Random.NextDouble() < 0.4,
-            //            "Swordsman" => true,
-            //            "Archer" => true,
-            //            _ => throw new ArgumentOutOfRangeException()
-            //        };
-            //        break;
-            //    case "Swordsman":
-            //        attackSuccess = defender.Type switch
-            //        {
-            //            "Horseman" => null,
-            //            "Swordsman" => true,
-            //            "Archer" => false,
-            //            _ => throw new ArgumentOutOfRangeException()
-            //        };
-            //        break;
-            //    case "Horseman":
-            //        attackSuccess = defender.Type switch
-            //        {
-            //            "Horseman" => true,
-            //            "Swordsman" => true,
-            //            "Archer" => false,
-            //            _ => throw new ArgumentOutOfRangeException()
-            //        };
-            //        break;
-            //    default:
-            //        throw new ArgumentOutOfRangeException();
-            //}
-
-            //if (attackSuccess == true)
-            //{
-            //    //defender.Health = Math.Max(defender.Health / 2, defender.InitialHealth / 4);
-            //    //if (defender.Health <= defender.InitialHealth / 4) defender.Health = 0;
-            //    defender.Health = 0;
-            //}
-            //else if (attackSuccess == false)
-            //{
-            //    //attacker.Health = Math.Max(attacker.Health / 2, attacker.InitialHealth / 4);
-            //    //if (attacker.Health <= attacker.InitialHealth / 4) attacker.Health = 0;
-            //    attacker.Health = 0;
-            //}
         }
     }
 }
